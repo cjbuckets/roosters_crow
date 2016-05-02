12ffababdda2451dbd18432ac1bd3ae51f33ac63
cjbuckets <cjbuckets@icloud.com>
2016-05-02 18:54:08 -0500

    first story

---
+++ git_story.rb
@@ -0,0 +1,4 @@
+Today I took my swapped my winter tires for all season tires. 
+For dinner I made sweet potatoes, chicken thighs, and garlic bread. 
+Then I hopped on my bike and pedaled to the Merchandise Mart. 
+Now I am eating delicious pistachios. 
